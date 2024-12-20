897030214ecac0f19bf7e4ddfcd6b16aa3c72e7f
kangeshan <Kangeshan.Manickavasagar@triosstudent.com>
2024-12-20 15:11:34 -0500

    add files

---
+++ css/app.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+const gallery = document.getElementById('gallery');
+  const scrollLeftButton = document.getElementById('scrollLeft');
+  const scrollRightButton = document.getElementById('scrollRight');
+
+  // Scroll left when the left button is clicked
+  scrollLeftButton.addEventListener('click', () => {
+    gallery.scrollLeft -= 200; // Adjust the value (200px) for scroll distance
+  });
+
+  // Scroll right when the right button is clicked
+  scrollRightButton.addEventListener('click', () => {
+    gallery.scrollLeft += 200; // Adjust the value (200px) for scroll distance
+  });
